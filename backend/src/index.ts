1dad9447b1c7d86969398a58fd57d1f34b624aad
dariodaddamio <dario.daddamio@gmail.com>
2024-09-28 10:54:18 -0700

    fix: npm dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Express!");
 });
 
 app.listen(port, () => {
