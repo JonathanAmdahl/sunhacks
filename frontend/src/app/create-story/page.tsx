45f55c5735a11f9317269b419f7eeebac8846815
Ronald McInnes <rjmcars8@gmail.com>
2024-09-28 17:02:36 -0700

    frontned

--- frontend/src/app/create-story/page.tsx
+++ frontend/src/app/create-story/page.tsx
@@ -1,8 +1,24 @@
+import Picture from "@/components/Picture";
+import Transcribes from "@/components/Transcribes";
+
 export default function CreateStory() {
     return (
-      <div>
-        <h1>About Us</h1>
-        <p>Welcome to our About page!</p>
+      <div className="bg-[#1E1E1E] h-screen w-screen items-center justify-center flex flex-col">
+        <div className="flex gap-10 w-screen px-[10%] h-[70%] justify-center items-center mt">
+          <Transcribes/>
+          <Picture/>
+        </div>
+        <div className='w-full px-[10%] flex justify-between mt-10'>
+          <button className='text-3xl font-black text-white'>
+            &lt; Previous Page
+          </button>
+          <button className='text-3xl font-black text-white bg-[#8E60C0]'>
+            Save Fable
+          </button>
+          <button className='text-3xl font-black text-white'>
+            Next Page &gt;
+          </button>
+        </div>
       </div>
     )
 }
---
+++ frontend/src/components/Picture.tsx
@@ -0,0 +1,3 @@
+export default function Transcribes() {
+    return (<div className="bg-white w-[50%] h-[100%]">g</div>)
+}
